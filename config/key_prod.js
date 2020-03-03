2cbd906363f712fe6f8fc7c2ff3333dfe3f02c0e
Ishak Ertem <ertemishakk@gmail.com>
2020-03-03 18:11:04 +1100

    bug fixed

--- config/key_prod.js
+++ config/key_prod.js
@@ -1,9 +1,9 @@
 module.exports = {
-    mongoURI: provess.env.MONGO_URI,
-    secret: provess.env.SECRET_OR_KEY,
-    secretAccessKey: provess.env.SECRET_ACCESS_KEY,
-    accessKeyId: provess.env.ACCESS_KEY_ID,
-    sendGridAPI: provess.env.SENDGRID,
+    mongoURI: process.env.MONGO_URI,
+    secret: process.env.SECRET_OR_KEY,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    sendGridAPI: process.env.SENDGRID,
 
 
 
