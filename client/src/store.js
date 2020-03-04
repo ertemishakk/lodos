a2858bf95d480233b2eb1595a3022d26e9081656
Ishak Ertem <ertemishakk@gmail.com>
2020-03-04 18:41:37 +1100

    redux middleware removed

--- client/src/store.js
+++ client/src/store.js
@@ -9,7 +9,7 @@ const initialState = {};
 const middleware = [thunk];
 
 const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
 export default store;
